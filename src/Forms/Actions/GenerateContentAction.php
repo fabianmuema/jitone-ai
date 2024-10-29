f443b529f07b69344d52cb20559787aa8dcf07a7
Fabian Muema <muemafabian@gmail.com>
2024-10-29 12:00:49 +0300

    Update GenerateContentAction.php

--- src/Forms/Actions/GenerateContentAction.php
+++ src/Forms/Actions/GenerateContentAction.php
@@ -2,13 +2,13 @@
 
 namespace Jiten14\JitoneAi\Forms\Actions;
 
+use App\Models\SmsPrompt;
 use Filament\Forms\Components\Actions\Action;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\Select;
 use Filament\Forms\Components\RichEditor;
-use Filament\Forms\Components\Toggle;
-use Jiten14\JitoneAi\JitoneAi;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
 use Filament\Notifications\Notification;
+use Jiten14\JitoneAi\JitoneAi;
 
 class GenerateContentAction
 {
@@ -25,13 +25,18 @@ class GenerateContentAction
                         'expand' => 'Expand',
                         'shorten' => 'Shorten',
                     ])
-                    ->visible(fn (callable $get) => $get('use_existing_content'))
-                    ->required(fn (callable $get) => $get('use_existing_content')),
+                    ->visible(fn(callable $get) => $get('use_existing_content'))
+                    ->required(fn(callable $get) => $get('use_existing_content')),
                 Textarea::make('ai_prompt')
                     ->label('Enter your prompt')
                     ->required()
-                    ->placeholder(fn ($get) => $get('ai_prompt_placeholder') ?? 'Write your sms prompt here. eg. Write a short sms notifying clients of a scheduled maintenance.')
-                    ->visible(fn (callable $get) => !$get('use_existing_content')),
+                    ->helperText('For best results, please enter a valid prompt. The limit is 5 prompts per day. You have '.(5 - SmsPrompt::where('tenant', \tenant('id'))->whereDate('created_at', now())->count()).' prompts left today. All prompts are stored for future reference.')
+                    ->placeholder(
+                        fn($get) => $get(
+                            'ai_prompt_placeholder',
+                        ) ?? 'Write your sms prompt here. eg. Write a short sms notifying clients of a scheduled maintenance.',
+                    )
+                    ->visible(fn(callable $get) => !$get('use_existing_content')),
 //                Select::make('template')
 //                    ->label('Or choose a template')
 //                    ->options(function () {
@@ -48,6 +53,19 @@ class GenerateContentAction
 //                    ->visible(fn (callable $get) => !$get('use_existing_content')),
             ])
             ->action(function (array $data) use ($field, $options) {
+                $prompts = SmsPrompt::where('tenant', \tenant('id'))
+                    ->whereDate('created_at', now())
+                    ->count();
+
+                if ($prompts > 5) {
+                    Notification::make()
+                        ->warning()
+                        ->title('Limit Exceeded')
+                        ->body('You have exceeded the limit of 5 prompts today. Please try again tomorrow.')
+                        ->send();
+                    return;
+                }
+
                 if (!env('OPENAI_API_KEY')) {
                     Notification::make()
                         ->warning()
@@ -60,45 +78,46 @@ class GenerateContentAction
                 try {
                     $currentContent = $field->getState();
 
-                        $prompt = $data['ai_prompt'] ?? null;
+                    $prompt = $data['ai_prompt'] ?? null;
 
-                        if (empty($prompt)) {
-                            throw new \Exception("Prompt is empty or null. Form data: " . json_encode($data));
-                        }
-
+                    if (empty($prompt)) {
+                        throw new \Exception("Prompt is empty or null. Form data: ".json_encode($data));
+                    }
 
                     $generatedContent = app(JitoneAi::class)->generateContent($prompt, $options);
-                    
+
                     $textInputContent = $generatedContent;
                     // Remove incomplete sentences
                     $generatedContent = $this->removeIncompleteSentences($generatedContent);
 
-                        // Append the new content to the existing content for non-existing content actions
-                        if ($field instanceof RichEditor) {
-                            $newContent = $currentContent . "\n\n" . $generatedContent;
-                        } elseif ($field instanceof Textarea) {
-                            $newContent = $currentContent . "\n" . $generatedContent;
-                        } else {
-                            $newContent = trim($currentContent . ' ' . $textInputContent);
-                        }
+                    // Append the new content to the existing content for non-existing content actions
+                    if ($field instanceof RichEditor) {
+                        $newContent = $currentContent."\n\n".$generatedContent;
+                    } elseif ($field instanceof Textarea) {
+                        $newContent = $currentContent."\n".$generatedContent;
+                    } else {
+                        $newContent = trim($currentContent.' '.$textInputContent);
+                    }
 
-                    
-                    // Set the new content
                     $field->state($newContent);
 
-                    // Notify the user of successful content generation
+                    SmsPrompt::create([
+                        'tenant' => \tenant('id'),
+                        'prompt' => $prompt,
+                        'result' => $generatedContent,
+                    ]);
+
                     Notification::make()
                         ->success()
                         ->title('Content Generated Successfully')
                         ->body('The AI-generated content has been added to the field.')
                         ->send();
-
                 } catch (\Exception $e) {
                     // Notify the user if an error occurs
                     Notification::make()
                         ->danger()
                         ->title('Error Generating Content')
-                        ->body('An error occurred while generating content: ' . $e->getMessage())
+                        ->body('An error occurred while generating content: '.$e->getMessage())
                         ->send();
                 }
             })
@@ -110,13 +129,13 @@ class GenerateContentAction
     {
         $sentences = preg_split('/(?<=[.!?])\s+/', $content, -1, PREG_SPLIT_NO_EMPTY);
         $lastSentence = end($sentences);
-        
+
         // Check if the last sentence ends with a period, exclamation mark, or question mark
         if (!preg_match('/[.!?]$/', $lastSentence)) {
             // Remove the last sentence if it's incomplete
             array_pop($sentences);
         }
-        
+
         return implode(' ', $sentences);
     }
 }
