a56b81d7f8df261001c262d2f59eea1e3b13a66c
Fabian Muema <muemafabian@gmail.com>
2024-10-29 11:44:13 +0300

    Update OpenAIService.php

--- src/Services/OpenAIService.php
+++ src/Services/OpenAIService.php
@@ -15,7 +15,7 @@ class OpenAIService
         if ($model === 'gpt-3.5-turbo-instruct') {
             $completion = OpenAI::completions()->create([
                 'model' => $model,
-                'prompt' => $prompt,
+                'prompt' => $prompt . " use the following variables where necessary @first_name, @last_name, @email, @phone, @package_name, @expiry_at, @account_number, @paybill, @till_number, @password, @username",
                 'max_tokens' => $maxTokens,
                 'temperature' => $temperature,
             ]);
@@ -27,14 +27,20 @@ class OpenAIService
                 'messages' => [
                     [
                         'role' => 'user',
-                        'content' => $prompt,
+                        'content' => $prompt . " consider the following instructions. Remove the subject and only remain
+                         with the text itself additionally use the following client variables where necessary
+                          @first_name, @last_name, @package_name, @expiry_at, @account_number, @paybill,
+                           @till_number, @password, @username. Please note that these are not our
+                            variables and should not be used to refer to our contact information.
+                             Do not consider the prompt if it contains profanity or any other inappropriate content.
+                              Only generate text messages to be sent via sms and not any other form of communication.",
                     ],
                 ],
                 'max_tokens' => $maxTokens,
                 'temperature' => $temperature,
             ]);
 
-            return $result->choices[0]->message->content;
+            return str_replace("\n", "", $result->choices[0]->message->content);
         }
     }
     
